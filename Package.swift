9602e79923c5e61e7fda56d26fa085c2f34f7fbf
Alex Fajkowski <alex@fajkowski.com>
2024-02-17 11:01:06 -0800

    Swift version 5.9. Added visionOS support. Removed test target dependencies.

--- Package.swift
+++ Package.swift
@@ -1,20 +1,17 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 
 import PackageDescription
 
-let webAuthPlatforms: [Platform] = [.iOS, .macOS, .macCatalyst]
+let webAuthPlatforms: [Platform] = [.iOS, .macOS, .macCatalyst, .visionOS]
 let swiftSettings: [SwiftSetting] = [.define("WEB_AUTH_PLATFORM", .when(platforms: webAuthPlatforms))]
 
 let package = Package(
     name: "Auth0",
-    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7)],
+    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7), .visionOS(.v1)],
     products: [.library(name: "Auth0", targets: ["Auth0"])],
     dependencies: [
-        .package(url: "https://github.com/auth0/SimpleKeychain.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/senstream/SimpleKeychain.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/auth0/JWTDecode.swift.git", .upToNextMajor(from: "3.1.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "12.0.0")),
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0"))
     ],
     targets: [
         .target(
@@ -26,16 +23,5 @@ let package = Package(
             path: "Auth0",
             exclude: ["Info.plist"],
             swiftSettings: swiftSettings),
-        .testTarget(
-            name: "Auth0Tests",
-            dependencies: [
-                "Auth0",
-                .product(name: "Quick", package: "Quick"),
-                .product(name: "Nimble", package: "Nimble"),
-                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs")
-            ],
-            path: "Auth0Tests",
-            exclude: ["Info.plist", "Auth0.plist"],
-            swiftSettings: swiftSettings)
     ]
 )
