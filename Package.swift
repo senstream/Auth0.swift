17ed5b6272eb5d4517bb252dad11c2287b822ffb
Alex Fajkowski <alex@fajkowski.com>
2024-02-17 20:05:09 -0800

    Point to 1.1.1 on fixed visionOS compatible SimpleKeychain

--- Package.swift
+++ Package.swift
@@ -10,15 +10,15 @@ let package = Package(
     platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7), .visionOS(.v1)],
     products: [.library(name: "Auth0", targets: ["Auth0"])],
     dependencies: [
-        .package(url: "https://github.com/senstream/SimpleKeychain.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/auth0/JWTDecode.swift.git", .upToNextMajor(from: "3.1.0")),
+        .package(url: "https://github.com/senstream/SimpleKeychain", .upToNextMajor(from: "1.1.1")),
     ],
     targets: [
         .target(
             name: "Auth0", 
             dependencies: [
-                .product(name: "SimpleKeychain", package: "SimpleKeychain"),
-                .product(name: "JWTDecode", package: "JWTDecode.swift")
+                .product(name: "JWTDecode", package: "JWTDecode.swift"),
+                .product(name: "SimpleKeychain", package: "SimpleKeychain")
             ],
             path: "Auth0",
             exclude: ["Info.plist"],
